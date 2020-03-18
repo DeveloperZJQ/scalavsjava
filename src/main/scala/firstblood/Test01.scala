bdd92af3884b6683fc925ad9c68b3e9ec9dfa3e8
zjq <904377260@qq.com>
2020-03-19 00:57:30 +0800

    first commit

---
+++ src/main/scala/firstblood/Test01.scala
@@ -0,0 +1,15 @@
+package firstblood
+
+/**
+ * @author happy
+ * @create 2020-03-18 23:57
+ */
+class Test01 {
+
+}
+
+object Test01{
+  def main(args: Array[String]): Unit = {
+    print("12321")
+  }
+}
