e54de84bf7a54b69b6ee95eee1e96c60ae683952
zjq <904377260@qq.com>
2020-03-19 01:01:55 +0800

    Java first commit

---
+++ src/main/java/firstblood/JTest01.java
@@ -0,0 +1,11 @@
+package firstblood;
+
+/**
+ * @author happy
+ * @create 2020-03-19 1:01
+ */
+public class JTest01 {
+    public static void main(String[] args) {
+        System.out.println("12321");
+    }
+}
