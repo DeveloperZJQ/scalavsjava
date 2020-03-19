1b85c0ad3037137aef24b3b04fcd9169dd6a412d
zjq <904377260@qq.com>
2020-03-19 16:21:41 +0800

    compare loop function about while and for use (java or scala)

--- src/main/java/firstblood/JTest01.java
+++ src/main/java/firstblood/JTest01.java
@@ -3,9 +3,25 @@ package firstblood;
 /**
  * @author happy
  * @create 2020-03-19 1:01
+ * compare loop function about while and for use java
  */
 public class JTest01 {
-    public static void main(String[] args) {
-        System.out.println("12321");
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(10000);
+        long start2 = System.currentTimeMillis();
+        int a =0;
+        while (a<=10000){
+            System.out.println("------------26------------>"+a);
+            a++;
+        }
+        long end2 = System.currentTimeMillis();
+        long start1 = System.currentTimeMillis();
+        for(int b=1; b<=10000;b++){
+            System.out.println("----------30-------------->"+b);
+        }
+        long end1 = System.currentTimeMillis();
+
+        System.out.println("end-start---------1-------->"+(end1-start1));
+        System.out.println("end-start---------2-------->"+(end2-start2));
     }
 }
--- src/main/scala/firstblood/Test01.scala
+++ src/main/scala/firstblood/Test01.scala
@@ -3,13 +3,34 @@ package firstblood
 /**
  * @author happy
  * @create 2020-03-18 23:57
+ * compare loop function about while and for use java
  */
 class Test01 {
-
+  def waterMelon(s: String): String ={
+    if(s.length>5){
+      "five"
+    }else{
+      "smaller then five"
+    }
+  }
 }
 
 object Test01{
+  private val t = new Test01()
   def main(args: Array[String]): Unit = {
-    print("12321")
+    val start1 = System.currentTimeMillis()
+    for(a<-1 to 1000){
+      println("---------463----------->"+a)
+    }
+    val end1    = System.currentTimeMillis()
+    val start2  = System.currentTimeMillis()
+    var a =1000;
+    while (a!=0){
+      println("---------443----------->"+a)
+      a=a-1
+    }
+    val end2   = System.currentTimeMillis()
+    println("end1-start1----------------->"+(end1-start1))
+    println("end2-start2----------------->"+(end2-start2))
   }
 }
