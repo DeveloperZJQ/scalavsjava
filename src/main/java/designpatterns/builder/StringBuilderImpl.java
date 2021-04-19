1047d1dbbcdf3cca739767ea90d78ec9529be929
happy <18380463469@163.com>
2021-04-19 08:51:50 +0800

    建造者更新

---
+++ src/main/java/designpatterns/builder/StringBuilderImpl.java
@@ -0,0 +1,12 @@
+package designpatterns.builder;
+
+/**
+ * @author happy
+ * @since 2021-04-18
+ */
+public class StringBuilderImpl {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println(stringBuilder);
+    }
+}
