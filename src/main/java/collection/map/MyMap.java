26d0b5183bd33d9777fcf6d6599ae2bfa69bf6f5
ZhangJianqiang <zjq@idatafun.com>
2022-04-24 09:43:09 +0800

    update mAP

---
+++ src/main/java/collection/map/MyMap.java
@@ -0,0 +1,15 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author DeveloperZJQ
+ * @since 2022-4-22
+ */
+public class MyMap {
+    public static void main(String[] args) {
+        HashMap<Object, Object> map = new HashMap<>();
+        ConcurrentHashMap<Object, Object> concurrentHashMap = new ConcurrentHashMap<>();
+    }
+}
